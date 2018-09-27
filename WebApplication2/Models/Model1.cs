4b2fe76899871784418d50797ea0b251fb709add
ifotn <rich@infrontofthenet.com>
2018-09-27 18:29:24 -0400

    Add project files.

---
+++ WebApplication2/Models/Model1.cs
@@ -0,0 +1,24 @@
+namespace WebApplication2.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Project>()
+                .Property(e => e.Name)
+                .IsUnicode(false);
+        }
+    }
+}
